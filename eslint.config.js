325485cedf26ea59183bfad298a133e29a2c95fa
Andrey <almakaevandrey99@gmail.com>
2025-07-16 21:18:08 +0300

    Add new Readme

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,7 @@ import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 import { react } from '@babel/types';
-import { version } from 'react';
+import prettier from 'eslint-config-prettier';
 
 export default tseslint.config(
   {
@@ -48,5 +48,6 @@ export default tseslint.config(
         version: 'detect',
       },
     },
+    prettier,
   }
 );
